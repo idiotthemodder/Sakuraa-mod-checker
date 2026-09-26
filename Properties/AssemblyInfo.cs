f9ac79c16edfd97e5c323219fe993e6fb3c92417
idiotthemodder <idiotslittlefemboy@proton.me>
2026-09-26 00:23:32 -0400

    auto sync 2026-09-26 00:23:32

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.Versioning;
 
 [assembly: AssemblyCompany("SakuraaForLinuxByIdiot")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyInformationalVersion("1.3.0")]
 [assembly: AssemblyProduct("Sakuraa For linux")]
 [assembly: AssemblyTitle("Sakuraa For linux - by idiot")]
 [assembly: AssemblyDescription("Sakuraa For linux - by idiot")]
 [assembly: AssemblyCopyright("")]
-[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyVersion("1.3.0.0")]
